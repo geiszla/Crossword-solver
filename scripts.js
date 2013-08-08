3509abf2fc674fee5e685d9684be99efdbf66f07
Geiszl András <geiszla@gmail.com>
2013-08-08 22:31:39 +0200

    Beállítások
    Beállítások gomb + "keresés visszafelé" opció hozzáadva.

--- scripts.js
+++ scripts.js
@@ -1,51 +1,142 @@
+"use strict";
+
 function init()
 {
-	var submitButton = document.getElementById("submit");
-	submitButton.addEventListener("click", main, false);
+	document.body.addEventListener("keydown", keyPressed, false);
+	document.getElementById("submit").addEventListener("click", checkData, false);
+	document.getElementById("settingsButton").addEventListener("click", changeSettings, false);
 }
 
-function sortCoords(outputWordPlace)
+function checkData()
 {
-	var temp = "0";
-	temp = outputWordPlace[outputWordPlace.length - 1][0];
-	outputWordPlace[outputWordPlace.length - 1][0] = outputWordPlace[outputWordPlace.length - 1][2]
-	outputWordPlace[outputWordPlace.length - 1][2] = temp;
+	var inputWords = [];
+	inputWords = document.getElementById("inputWords").value.toUpperCase().replace(/ /g, "").split(",");
+	window.inputWords = inputWords;
 
-	temp = outputWordPlace[outputWordPlace.length - 1][1];
-	outputWordPlace[outputWordPlace.length - 1][1] = outputWordPlace[outputWordPlace.length - 1][3]
-	outputWordPlace[outputWordPlace.length - 1][3] = temp;
+	var inputText = [];
+	inputText = document.getElementById("inputText").value.replace(/	/g, "").toUpperCase().split("\n");
+	window.inputText = inputText;
+
+	var mainBody = document.body.innerHTML;
+	window.mainBody = mainBody;
+
+	var isSettingsOpen = false;
+	window.isSettingsOpen = isSettingsOpen;
+
+	if (inputText[0] === "" && inputWords[0] === "")
+	{
+		alert("Nincsenek beírva adatok.");
+	}
+
+	else if (inputText[0] === "")
+	{
+		alert("Nincs táblázat, amiben kereshetnék.");
+	}
+
+	else if (inputWords[0] === "")
+	{
+		alert("Nincsenek keresési kulcsszavak.");
+	}
+
+	else
+	{
+		var isEqual = true;
+
+		for (var i = 0; i < inputText.length - 1; i++)
+		{
+			if (inputText[i].length !== inputText[i + 1].length)
+			{
+				isEqual = false;
+			}
+		}
+
+		if (isEqual = false)
+		{
+			alert("A táblázat sorai nem egyenlő hosszúak.");
+		}
+
+		else
+		{
+			main();
+		}
+	}
 }
 
-function goBack()
+function main()
 {
-	document.body.innerHTML = '<div id="main"> <p>Keresendő szavak (vesszővel elválasztva):<br /><input type="text" id="inputWords" size="50" /></p> <p>Táblázat:<br /><textarea id="inputText" rows="10" cols="10"></textarea></p> <p><input type="button" id="submit" value="Keresés!" /></p></div>'
+	var isBackwards = document.getElementById("isBackwards").checked;
+	window.isBackwards = isBackwards;
+
+	var wordsNotFound = [];
+	window.wordsNotFound = wordsNotFound;
 
 	for (var i = 0; i < inputWords.length; i++)
 	{
-		document.getElementById("inputWords").value += inputWords[i].toLowerCase();
-
-		if (i !== inputWords.length - 1)
-		{
-			document.getElementById("inputWords").value += ", ";
-		}
+		wordsNotFound[i] = inputWords[i];
 	}
 
-	for (var i = 0; i < inputText.length; i++)
+	buildTable();
+
+	for (var i = 0; i < inputWords.length; i++)
 	{
-		document.getElementById("inputText").value += inputText[i].toLowerCase();
+		findWord(inputWords[i], "horizontal", inputText.length, inputText[0].length - inputWords[i].length + 1, 0);
+		findWord(inputWords[i], "vertical", inputText.length - inputWords[i].length + 1, inputText[0].length, 0);
+		findWord(inputWords[i], "rightDown", inputText.length - inputWords[i].length + 1, inputText[0].length - inputWords[i].length + 1, 0);
+		findWord(inputWords[i], "leftDown", inputText.length - inputWords[i].length + 1, inputText[0].length, inputWords[i].length - 1);
+	}
 
-		if (i !== inputText.length - 1)
+	if (wordsNotFound.length === inputWords.length)
+	{
+		document.getElementById("main").innerHTML = '<p style="color: red;">Egyetlen szó sem volt megtalálható a táblázatban. Ellenőrizd a megadott adatokat és a beállításokat!</p><p><input type="button" id="back" value = "<- Vissza"><input type="button" id="settingsButton" value="Beállítások ↑" /></p><p id="settings" style="display: none;"><input type="checkbox" id="isBackwards" checked="true" /> Keresés visszafelé</p>';
+
+		document.getElementById("back").addEventListener("click", goBack, false);
+	}
+
+	else
+	{
+		if (wordsNotFound.length === 0)
 		{
-			document.getElementById("inputText").value += "\n";
+			document.getElementById("notFound").innerHTML += "Minden megadott szó megtalálható a táblázatban.";
+		}	
+
+		else if (wordsNotFound.length === 1)
+		{
+			document.getElementById("isNotFound").innerHTML += "A következő szó nem található a táblázatban:";
+		}
+
+		else
+		{
+			document.getElementById("isNotFound").innerHTML += "A következő szavak nem találhatók a táblázatban:";
+		}
+
+		for (var i = 0; i < wordsNotFound.length; i++)
+		{
+			document.getElementById("notFound").innerHTML += wordsNotFound[i].toLowerCase();
+
+			if (i !== wordsNotFound.length - 1)
+			{
+				document.getElementById("notFound").innerHTML += ", ";
+			}
 		}
 	}
 
-	document.getElementById("submit").addEventListener("click", main, false);
+	document.getElementById("settingsButton").addEventListener("click", changeSettings, false);
+	document.getElementById("isBackwards").addEventListener("change", main, false);
+
+	if (isSettingsOpen === true)
+	{
+		changeSettings("open");
+	}
+
+	if (isBackwards !== true)
+	{
+		document.getElementById("isBackwards").checked = false;
+	}
 }
 
 function buildTable()
 {
-	document.body.innerHTML = '<div id="main"> Megtalált szavak: <p><table id="outputTable" style="margin:auto; font-size:20px; border-collapse: collapse;"></table></p> <p><input type="button" id="back" value = "<- Vissza"></p> </div>'
+	document.body.innerHTML = '<div id="main"><div id="head">A táblázatban megtalált szavak:</div> <p></p><table id="outputTable" style="margin:auto; font-size:20px; border-collapse: collapse;"></table> <br><div id="isNotFound"></div><div id="notFound" style="color: red;"></div> <p><input type="button" id="back" value = "<- Vissza"><input type="button" id="settingsButton" value="Beállítások ↑" /></p><p id="settings" style="display: none;"><input type="checkbox" id="isBackwards" checked="true" /> Keresés visszafelé</p> </div>'
 
 	document.getElementById("back").addEventListener("click", goBack, false);
 
@@ -102,13 +193,6 @@ function findWord(inputWord, direction, bottomBorder, rightBorder, startCoordX)
 					var firstCoord = i + k;
 					var secondCoord = j - k;
 				}
-				
-				/*
-				if (direction === "horizontal")
-				{
-					console.log(firstCoord + ", " + secondCoord);
-				}
-				*/
 
 				outputWord += document.getElementById("td" + String(firstCoord) + "|" + String(secondCoord)).innerHTML;
 
@@ -126,9 +210,42 @@ function findWord(inputWord, direction, bottomBorder, rightBorder, startCoordX)
 				k++;
 			}
 
-			if (outputWord !== inputWord && outputWord.split("").reverse().join("") !== inputWord)
+			if (isBackwards === true)
 			{
-				outputWordPlace.splice(outputWordPlace.length - 1, 1);
+				if (outputWord !== inputWord && outputWord.split("").reverse().join("") !== inputWord)
+				{
+					outputWordPlace.splice(outputWordPlace.length - 1, 1);
+				}
+
+				else
+				{
+					for (var k = 0; k < wordsNotFound.length; k++)
+					{
+						if (wordsNotFound[k] === outputWord || wordsNotFound[k] === outputWord.split("").reverse().join(""))
+						{
+							wordsNotFound.splice(k, 1);
+						}
+					}
+				}
+			}
+
+			else
+			{
+				if (outputWord !== inputWord)
+				{
+					outputWordPlace.splice(outputWordPlace.length - 1, 1);
+				}
+
+				else
+				{
+					for (var k = 0; k < wordsNotFound.length; k++)
+					{
+						if (wordsNotFound[k] === outputWord || wordsNotFound[k] === outputWord.split("").reverse().join(""))
+						{
+							wordsNotFound.splice(k, 1);
+						}
+					}
+				}
 			}
 
 			if (outputWordPlace[outputWordPlace.length - 1] !== undefined)
@@ -152,10 +269,10 @@ function findWord(inputWord, direction, bottomBorder, rightBorder, startCoordX)
 		}
 	}
 
-	for (var j = 0; j < outputWordPlace.length; j++)
+	for (var i = 0; i < outputWordPlace.length; i++)
 	{
-		showResult(outputWordPlace[j][0], outputWordPlace[j][1], outputWordPlace[j][2], outputWordPlace[j][3]);
-	}
+		showResult(outputWordPlace[i][0], outputWordPlace[i][1], outputWordPlace[i][2], outputWordPlace[i][3]);
+	}	
 }
 
 function showResult(startCoordX, startCoordY, endCoordX, endCoordY)
@@ -235,59 +352,75 @@ function showResult(startCoordX, startCoordY, endCoordX, endCoordY)
 	}
 }
 
-function main()
+function goBack()
 {
-	var inputWords = [];
-	inputWords = document.getElementById("inputWords").value.toUpperCase().replace(/ /g, "").split(",");
-	window.inputWords = inputWords;
+	document.body.innerHTML = mainBody;
 
-	var inputText = [];
-	inputText = document.getElementById("inputText").value.replace(/	/g, "").toUpperCase().split("\n");
-	window.inputText = inputText;
-
-	if (inputText[0] === "" && inputWords[0] === "")
+	for (var i = 0; i < inputWords.length; i++)
 	{
-		alert("Nincsenek beírva adatok.");
-	}
+		document.getElementById("inputWords").value += inputWords[i].toLowerCase();
 
-	else if (inputText[0] === "")
-	{
-		alert("Nincs táblázat, amiben kereshetnék.");
-	}
-
-	else if (inputWords[0] === "")
-	{
-		alert("Nincsenek keresési kulcsszavak.");
-	}
-
-	else
-	{
-		var isEqual = true;
-
-		for (var i = 0; i < inputText.length - 1; i++)
+		if (i !== inputWords.length - 1)
 		{
-			if (inputText[i].length !== inputText[i + 1].length)
-			{
-				isEqual = false;
-			}
-		}
-
-		if (isEqual = false)
-		{
-			alert("A táblázat sorai nem egyenlő hosszúak.");
-		}
-
-		else
-		{
-			buildTable();
-
-			for (var i = 0; i < inputWords.length; i++)
-			{
-				findWord(inputWords[i], "horizontal", inputText.length, inputText[0].length - inputWords[i].length + 1, 0);
-				findWord(inputWords[i], "vertical", inputText.length - inputWords[i].length + 1, inputText[0].length, 0);
-				findWord(inputWords[i], "rightDown", inputText.length - inputWords[i].length + 1, inputText[0].length - inputWords[i].length + 1, 0);
-				findWord(inputWords[i], "leftDown", inputText.length - inputWords[i].length + 1, inputText[0].length, inputWords[i].length - 1);
-			}
+			document.getElementById("inputWords").value += ", ";
 		}
 	}
+
+	for (var i = 0; i < inputText.length; i++)
+	{
+		document.getElementById("inputText").value += inputText[i].toLowerCase();
+
+		if (i !== inputText.length - 1)
+		{
+			document.getElementById("inputText").value += "\n";
+		}
+	}
+
+	document.getElementById("submit").addEventListener("click", checkData, false);
+	document.getElementById("settingsButton").addEventListener("click", changeSettings, false);
+	document.getElementById("isBackwards").checked = isBackwards;
+
+	changeSettings("close");
+}
+
+function keyPressed(e)
+{
+	if (e.keyCode == 13 && document.getElementById("submit") !== null && document.activeElement !== document.getElementById("inputText"))
+	{
+		checkData();
+	}
+
+	else if (e.keyCode == 8 && document.getElementById("back") !== null)
+	{
+		goBack();
+	}
+}
+
+function changeSettings(currState)
+{
+	if (document.getElementById("settingsButton").value === "Beállítások ↑" || currState === "close")
+	{
+		document.getElementById("settings").style.cssText = "display: normal;";
+		document.getElementById("settingsButton").value = "Beállítások ↓";
+		isSettingsOpen = true;
+	}
+
+	else if (document.getElementById("settingsButton").value === "Beállítások ↓" || currState === "open")
+	{
+		document.getElementById("settings").style.cssText = "display: none;";
+		document.getElementById("settingsButton").value = "Beállítások ↑";
+		isSettingsOpen = false;
+	}
+}
+
+function sortCoords(outputWordPlace)
+{
+	var temp = "0";
+	temp = outputWordPlace[outputWordPlace.length - 1][0];
+	outputWordPlace[outputWordPlace.length - 1][0] = outputWordPlace[outputWordPlace.length - 1][2]
+	outputWordPlace[outputWordPlace.length - 1][2] = temp;
+
+	temp = outputWordPlace[outputWordPlace.length - 1][1];
+	outputWordPlace[outputWordPlace.length - 1][1] = outputWordPlace[outputWordPlace.length - 1][3]
+	outputWordPlace[outputWordPlace.length - 1][3] = temp;
 }
